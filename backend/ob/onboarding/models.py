3dc79a2df1f4ef8683eb8a0496c652f4865d9fd2
tkay <taimor.khan@mail.utoronto.ca>
2024-09-17 19:27:42 -0400

    tailwind

--- backend/ob/onboarding/models.py
+++ backend/ob/onboarding/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Header(models.Model):
+
+    # name mapping
+    name1 = models.CharField(max_length=32)
+    name2 = models.CharField(max_length=32)
+    name3 = models.CharField(max_length=32)
+    name4 = models.CharField(max_length=32)
+
+    # type mapping
+    type1 = models.CharField(max_length=32)
+    type2 = models.CharField(max_length=32)
+    type3 = models.CharField(max_length=32)
+    type4 = models.CharField(max_length=32)
+
+    # temp
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Table(models.Model):
+    name = models.CharField(max_length=100)
+    owned_by = models.ForeignKey("User", on_delete=models.CASCADE)
+    file = models.FileField(upload_to="uploads/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    header = models.ForeignKey("Header", null=True, blank=True, on_delete=models.SET_NULL, related_name="header_tables")
+
+
+class Row(models.Model):
+    table = models.ForeignKey("Table", on_delete=models.CASCADE, related_name="table_rows")
+    col1 = models.CharField(max_length=100)
+    col2 = models.CharField(max_length=100)
+    col3 = models.CharField(max_length=100)
+    col4 = models.CharField(max_length=100)
--- backend/ob/onboarding/views.py
+++ backend/ob/onboarding/views.py
@@ -1,4 +1,43 @@
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Table, Row, Header
+
+def LoginUser(request):
+    name = request.body.get("name", None)
+    email = request.body.get("email", None)
+
+    return Response()
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return Response()
+
+
+def get_tables(request):
+
+    tables_qs = Table.objects.filter(owned_by_id=request.user.id)
+
+    return Response(data=tables_qs)
+
+
+def get_headers(request):
+
+    user_id = request.body.get("user", None)
+
+    if not user_id:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    headers_qs = Header.objects.prefetch_related("header_tables").filter(header_tables__owned_by_id=user_id)
+
+    return Response(data=headers_qs)
+
+def get_rows(request):
+    # get the header first, then send out data as the int type
+    table_id = request.body.get("table_id", None)
+    if not table_id:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # check if owned by user
+    rows = Row.objects.select_related("table").filter(table_id=table_id)
+
+    return Response(data=rows)
